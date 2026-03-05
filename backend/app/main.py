246328998f2fb6ca4749215da9af30d79af6bdab
Karthik Das P <karthikpilakat@gmail.com>
2026-03-05 21:10:27 +0530

    Added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.all_cors_origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type", "Accept", "X-Request-ID"],
 )
 
 # Create database tables on startup
