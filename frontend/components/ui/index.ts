acccdbbb880ac014011cd86e8da45d85156d736a
Karthik Das P <karthikpilakat@gmail.com>
2026-05-04 23:40:32 +0530

    Integrating shadcn

---
+++ frontend/components/ui/index.ts
@@ -0,0 +1,11 @@
+export { Button, buttonVariants } from "./button"
+export {
+	Card,
+	CardHeader,
+	CardFooter,
+	CardTitle,
+	CardAction,
+	CardDescription,
+	CardContent,
+} from "./card"
+export { Tabs, TabsList, TabsTrigger, TabsContent } from "./tabs"
